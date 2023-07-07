2c3fcda44aafeb279ea78af0c8c9da264cb4a0b6
Pale Kiwi <contact@palekiwi.com>
2023-07-07 19:11:08 +0800

    add mqtt_encode_length

--- src/mqtt.c
+++ src/mqtt.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mqtt.h"
+#include "pack.h"
 
 static size_t unpack_mqtt_connect(const unsigned char *,
                                   union mqtt_header *,
@@ -27,3 +28,32 @@ static unsigned char *pack_mqtt_ack(const union mqtt_packet *);
 static unsigned char *pack_mqtt_connack(const union mqtt_packet *);
 static unsigned char *pack_mqtt_suback(const union mqtt_packet *);
 static unsigned char *pack_mqtt_publish(const union mqtt_packet *);
+
+/*
+ * MQTT v3.1.1 standard, Remaining length field on the fixed header can be at
+ * most 4 bytes.
+ */
+
+static const int MAX_LEN_BYTES = 4;
+
+/*
+ * Encode Remaining Length on a MQTT packet header, comprised of Variable
+ * Header and Payload if present. It does not take into account the bytes
+ * required to store itself. Refer to MQTT v3.1.1 algorithm for the
+ * implementation.
+ */
+
+int mqtt_encode_length(unsigned char *buf, size_t len) {
+    int bytes = 0;
+    do {
+        if (bytes + 1 > MAX_LEN_BYTES)
+            return bytes;
+        short d = len % 128;
+        len /= 128;
+        /* if there are more digits to encode, set the top bit of this digit */
+        if (len > 0)
+            d |= 128;
+        buf[bytes++] = d;
+    } while (len > 0);
+    return bytes;
+}
--- src/pack.h
+++ src/pack.h
@@ -14,10 +14,10 @@ uint8_t unpack_u8(const uint8_t **);
 uint16_t unpack_u16(const uint8_t **);
 
 //bytes -> uint32_t
-uint16_t unpack_u32(const uint8_t **);
+uint32_t unpack_u32(const uint8_t **);
 
 // read a defined len of bytes
-uint8_t *unpack_bytes(const uint8_t, size_t, uint8_t);
+uint8_t *unpack_bytes(const uint8_t **, size_t, uint8_t*);
 
 // unpack a string prefixed by its length as uint16 value
 uint16_t unpack_string16(uint8_t **buf, uint8_t **dest);
@@ -27,10 +27,10 @@ uint16_t unpack_string16(uint8_t **buf, uint8_t **dest);
 void pack_u8(uint8_t **, uint8_t);
 
 // apppend a uint16_t -> bytes into the bytestring
-void pack_u16(uint16_t **, uint16_t);
+void pack_u16(uint8_t **, uint16_t);
 
 // apppend a uint32_t -> bytes into the bytestring
-void pack_u32(uint32_t **, uint32_t);
+void pack_u32(uint8_t **, uint32_t);
 
 // append len bytes into the bytestring
 void pack_bytes(uint8_t **, uint8_t *);
